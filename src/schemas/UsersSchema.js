b75b1fbed974df39b579bd53fb43dad80a15b734
baxacsharp <78537820+baxacsharp@users.noreply.github.com>
2021-12-20 18:26:16 +0100

    Update UsersSchema.js

--- src/schemas/UsersSchema.js
+++ src/schemas/UsersSchema.js
@@ -7,7 +7,7 @@ const UserSchema = new Schema(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true},
     merchant: {
       type: Schema.Types.ObjectId,
       ref: "Merchant",
